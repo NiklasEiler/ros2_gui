89107f73452032fcd5fc7c919eaab6a636025d91
NiklasEiler <niklaseilersausheinde@gmail.com>
2024-01-03 15:17:53 +0100

    GUI Bilder und §D Button

--- build/my_gui/build/lib/my_gui/my_gui_node.py
+++ build/my_gui/build/lib/my_gui/my_gui_node.py
@@ -1,84 +1,88 @@
-#!/usr/bin/env python3
-
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QTabWidget, QLabel
-from PyQt5.QtGui import QImage, QPixmap
-from PyQt5.QtCore import Qt
 import rclpy
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
+import time
+import sys
+from threading import Thread
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.node import Node
+from std_msgs.msg import String
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QWidget, QPushButton
 
-class MyGuiNode(QWidget):
 
+class GuiNode(Node):
     def __init__(self):
-        super().__init__()
+        super().__init__("simple_gui_node")
+        self.counter_    = 0
+        self.button1_publisher_  = self.create_publisher(String, '/gui/button_1', 10)
+        self.button2_publisher_  = self.create_publisher(String, '/gui/button_2', 10)
 
-        # ROS setup
-        self.node = rclpy.create_node('my_gui_node')
-        self.image_sub = self.node.create_subscription(Image, '/image_topic', self.image_callback, 10)
-        self.bridge = CvBridge()
 
-        # GUI setup
-        self.init_ui()
+    def button_1_publisher_callback_(self):
+        self.counter_ = self.counter_ + 1
+        msg = String()
+        msg.data = "Button 1 pressed"
+        self.button1_publisher_.publish(msg)
 
-    def init_ui(self):
-        self.setWindowTitle('ROS 2 GUI Node')
+    def button_2_publisher_callback_(self):
+        self.counter_ = self.counter_ + 1
+        msg = String()
+        msg.data = "Button 2 pressed"
+        self.button2_publisher_.publish(msg)
 
-        layout = QVBoxLayout(self)
+    def button_3_publisher_callback_(self):
+        self.counter_ = self.counter_ + 1
+        msg = String()
+        msg.data = "Button 3 pressed"
+        self.button2_publisher_.publish(msg)
 
-        # Buttons
-        button1 = QPushButton('Button 1', self)
-        button2 = QPushButton('Button 2', self)
+    def subscriber_callback_(self, msg):
+        self.get_logger().info("Button clicked, " + msg.data)
 
-        layout.addWidget(button1)
-        layout.addWidget(button2)
 
-        # Tabs
-        tabs = QTabWidget()
+    
+    
 
-        tab1 = QWidget()
-        tab2 = QWidget()
 
-        tabs.addTab(tab1, 'Tab 1')
-        tabs.addTab(tab2, 'Tab 2')
+    
+    
 
-        # Labels for displaying values
-        label_tab1 = QLabel('Value in Tab 1', self)
-        label_tab2 = QLabel('Value in Tab 2', self)
-
-        tab1_layout = QVBoxLayout(tab1)
-        tab1_layout.addWidget(label_tab1)
-
-        tab2_layout = QVBoxLayout(tab2)
-        tab2_layout.addWidget(label_tab2)
-
-        # Image display
-        self.image_label = QLabel(self)
-        self.image_label.setAlignment(Qt.AlignCenter)  # Center-align the image
-        layout.addWidget(tabs)
-        layout.addWidget(self.image_label)
-
-        self.setLayout(layout)
-        self.show()
-
-    def image_callback(self, msg):
-        cv_image = self.bridge.imgmsg_to_cv2(msg, 'bgr8')
-        height, width, channel = cv_image.shape
-        bytes_per_line = 3 * width
-        q_image = QImage(cv_image.data, width, height, bytes_per_line, QImage.Format_RGB888)
-        self.image_label.setPixmap(QPixmap.fromImage(q_image))
 
 def main(args=None):
     rclpy.init(args=args)
-
+    ros_node=GuiNode()
     app = QApplication(sys.argv)
-    gui_node = MyGuiNode()
-
+    executor = MultiThreadedExecutor()        
+    print("HMI preparation start")
     try:
-        sys.exit(app.exec_())
-    finally:
-        gui_node.node.destroy_node()
-        rclpy.shutdown()
+        gui = QMainWindow()
 
+        gui.setWindowTitle("Test ROS2 GUI")
+        gui.setGeometry(100, 100, 240, 240)
+        #gui.move(100, 100)
+
+        value_label = 'Value'
+        button1 = QPushButton("Pointcloud", parent=gui)
+        button1.move(60, 10)
+        button1.clicked.connect(ros_node.button_1_publisher_callback_)
+
+        button2 = QPushButton("Rechtes Bild", parent=gui)
+        button2.move(60, 60)
+        button2.clicked.connect(ros_node.button_2_publisher_callback_)
+
+        button2 = QPushButton("Linkes Bild", parent=gui)
+        button2.move(60, 110)
+        button2.clicked.connect(ros_node.button_3_publisher_callback_)
+
+        gui.show()
+        print("HMI preparation complete")
+        #rclpy.spin(ros_node_point)
+        sys.exit(app.exec())
+
+
+    finally:
+        ros_node.get_logger().info("Shutting down")
+        ros_node.destroy_node()
+        executor.shutdown()
+    
 if __name__ == '__main__':
     main()
--- install/setup.ps1
+++ install/setup.ps1
@@ -24,6 +24,7 @@ function _colcon_prefix_chain_powershell_source_script {
 # source chained prefixes
 _colcon_prefix_chain_powershell_source_script "/opt/ros/humble\local_setup.ps1"
 _colcon_prefix_chain_powershell_source_script "/home/niklas/ros2-stereo-camera-analyse/install\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/niklas/ros_3d/install\local_setup.ps1"
 
 # source this prefix
 $env:COLCON_CURRENT_PREFIX=(Split-Path $PSCommandPath -Parent)
